d324973745fb8c1835a6b81e17f7d6cabe383d82
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-03 11:36:53 +0800

    implements the upload feature

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ImageUpload extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageFile: null,
+    };
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+
+    const data = new FormData();
+    const img = e.target.files[0];
+
+    data.append('upload', img);
+
+    this.setState({
+      imageFile: data,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    const server = axios.create({
+      baseURL: 'http://localhost:8000',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    
+    server.post('/uploads', this.state.imageFile);
+  }
+
   render() {
     return (
       <div className="form">
         <h3>Form</h3>
-        <input type="file" className="form-control"
-          placeholder="Upload Your Images" name="upload" />
-          <button id="submit" className="btn btn-default">upload</button>
-          {/* <!-- Upload Form here --> */}
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="file" className="form-control"
+            onChange={this.handleChange.bind(this)}
+            placeholder="Upload Your Images" name="upload" />
+          <input type="submit" className="btn btn-default" />
+        </form>
       </div>
     );
   }
