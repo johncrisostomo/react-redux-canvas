16089e6ea7b6cbacc8eeccaf07f3edf8034d6ce0
John Crisostomo <johncrisostomo@protonmail.ch>
2017-06-04 08:53:29 +0800

    implements state persistence through localstorage

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ import Canvas from './components/Canvas';
 import ItemsList from './components/ItemsList';
 
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
 
 import './App.css';
 
 class App extends Component {
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    const persistedState = loadState();
+    const store = createStore(reducers, persistedState,
+      applyMiddleware(ReduxThunk));
+
+    store.subscribe(() => saveState(store.getState()));
+    
     return (
       <Provider store={store}>
         <div>
